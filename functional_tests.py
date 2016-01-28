33a0040e208c72766fd17df004de0b49d7228dbb
Guoozz <csguozhen@gmail.com>
2016-01-28 11:30:44 +0800

    functional test:增加并显示待办事项

--- functional_tests.py
+++ functional_tests.py
@@ -15,5 +15,23 @@ class NewVistorTest(unittest.TestCase):
         self.browser.get("http://127.0.0.1:8000")
         self.assertIn('To-Do',self.browser.title)
 
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do',header_text)
+
+        inputbox = self.browser.find_element_by_id('new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('do something...')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:do something...' for row in rows)
+        )
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
